9aa424c3d4e0247afb4b7807300c4c2fbb0b8fed
Khadar Basha <89116722+khadar099@users.noreply.github.com>
2026-04-20 00:31:42 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
   title = 'kb-fashion-house-frontend';
+
+  message: string = '';
+
+  constructor(private http: HttpClient) {}
+
+  getHomeMessage() {
+    this.http.get('http://98.130.53.119:8181/api/home', { responseType: 'text' })
+      .subscribe({
+        next: (res) => {
+          this.message = res;
+        },
+        error: (err) => {
+          console.log(err);
+          this.message = 'Backend connection failed ❌';
+        }
+      });
+  }
 }
