9dae16f68d75b3c38b8fa9f9679cc8972a71423f
Khadar Basha <89116722+khadar099@users.noreply.github.com>
2026-04-20 01:05:39 +0530

    Update register.component.ts

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
 
+  email: string = '';
+  password: string = '';
+
+  register() {
+    console.log('Register:', this.email, this.password);
+  }
 }
