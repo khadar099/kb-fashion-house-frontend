479c507e767f1cd466464992c0ad0432276cb765
Khadar Basha <89116722+khadar099@users.noreply.github.com>
2026-04-20 08:29:33 +0530

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,6 +10,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
+     provideForms(),
     importProvidersFrom(FormsModule)
   ]
 };
