831bbb2de13f197306cb4fafd1d1b1466fb2e9fd
Khadar Basha <89116722+khadar099@users.noreply.github.com>
2026-04-20 07:45:29 +0530

    Update register.component.ts

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,21 +1,42 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-register',
-  standalone: true,
-  imports: [FormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
 
-  email: string = '';
-  password: string = '';
+  emailOrMobile = '';
+  password = '';
+  confirmPassword = '';
+
+  constructor(
+    private api: ApiService,
+    private router: Router
+  ) {}
 
   register() {
-    console.log('Register:', this.email, this.password);
+
+    if (this.password !== this.confirmPassword) {
+      alert('Passwords do not match');
+      return;
+    }
+
+    this.api.register({
+      emailOrMobile: this.emailOrMobile,
+      password: this.password
+    }).subscribe({
+      next: () => {
+        alert('Registration successful');
+        this.router.navigate(['/login']);
+      },
+      error: (err) => {
+        console.log(err);
+        alert('Registration failed');
+      }
+    });
   }
 }
