03dd5d492efe68d162f1f887d83336e24eb86884
Khadar Basha <89116722+khadar099@users.noreply.github.com>
2026-04-20 07:33:56 +0530

    Update register.component.ts

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-register',
