ed4f427bb8595824301cbf3e01a26aab4dba1b75
Khadar Basha <89116722+khadar099@users.noreply.github.com>
2026-04-20 08:30:15 +0530

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { provideForms } from '@angular/forms';
 
 import { routes } from './app.routes';
 
