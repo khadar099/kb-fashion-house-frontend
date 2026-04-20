8036085a68ffb4ba0e08259541dcd52154e956e6
Khadar Basha <89116722+khadar099@users.noreply.github.com>
2026-04-20 08:14:44 +0530

    Update login.component.ts

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,41 +1,33 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
 import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
 
-  emailOrMobile = '';
-  password = '';
+  email: string = '';
+  password: string = '';
 
-  constructor(
-    private api: ApiService,
-    private router: Router
-  ) {}
+  constructor(private api: ApiService) {}
 
   login() {
-  const request = {
-    email: this.email,
-    password: this.password
-  };
+    const request = {
+      email: this.email,
+      password: this.password
+    };
 
-  this.api.login(request).subscribe({
-    next: (res) => {
-      alert('Login successful');
-      console.log(res);
-    },
-    error: (err) => {
-      alert('Invalid credentials');
-      console.log(err);
-    }
-  });
-}
+    this.api.login(request).subscribe({
+      next: (res) => {
+        alert('Login successful');
+        console.log(res);
+      },
+      error: (err) => {
+        alert('Invalid credentials');
+        console.log(err);
+      }
+    });
   }
+}
